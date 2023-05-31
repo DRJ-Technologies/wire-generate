746de7dd2737d6a5c8b1eb35dfa064a15476e12e
dan-drj <dan@drj.tools>
2023-05-31 15:47:02 -0400

    run only one directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Read the directories JSON array from the input and convert it to a list
-directories=$(echo "$1" | jq -r '.[]')
-
-# Iterate over the directories
-for dir in $directories; do
-    if [ -d "${dir}" ]; then
-        echo "Running wire on ${dir}"
-        wire ${dir}/...
-    else
-        echo "${dir} is not a directory"
-    fi
-done
+if [ -d $@ ]; then
+    echo "Running wire on $@"
+    wire $@/...
+else
+    echo "$@ is not a directory"
+fi
